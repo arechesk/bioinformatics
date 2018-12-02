7aab4f9957ada22a47167c8e2e9e7d26bbe3d3fe
aleksandrkulikov <unnotigkeit@mail.ru>
2018-12-03 01:43:28 +0300

    4

--- 4.Motif Finding/MedianStringProblem.py
+++ 4.Motif Finding/MedianStringProblem.py
@@ -1,2 +1,31 @@
+def hamming_distance(pattern, motif):
+    return sum([1 for i in range(len(motif)) if pattern[i]!=motif[i]])
+
+
+def k_mer_patterns(k):
+    patterns = ["A", "C", "G", "T"]
+    while(len(patterns[0]) < k):
+        patterns = [patterns[l]+j for j in ["A", "C", "G", "T"] for l in range(0, len(patterns))]
+    return patterns
+
+
+def min_distance(pattern, text):
+    k = len(pattern)
+    return min([hamming_distance(pattern, text[i:i+k]) for i in range(len(text) - k + 1)])
+
+
+def distance_between_pattern_and_dna(pattern, DNA):
+    return sum([min_distance(pattern, DNA_string) for DNA_string in DNA])
+
+
+def median_string(k, DNA):
+    patterns = k_mer_patterns(k)
+    median={distance_between_pattern_and_dna(pattern, DNA):pattern for pattern in patterns}
+    return median[min(median.keys())]
+
+
 if __name__ == "__main__":
-    pass
+    k=int(input())
+    DNA = [input() for i in range(10)]
+    print(median_string(k, DNA))
+
--- 4.Motif Finding/MotifEnumerationProblem.py
+++ 4.Motif Finding/MotifEnumerationProblem.py
@@ -1,2 +1,37 @@
+from functools import reduce
+
+
+def hamming_distance(pattern, motif):
+    return sum([1 for i in range(len(motif)) if pattern[i]!=motif[i]])
+
+
+def is_pattern_with_at_most_d_mistakes_in_string(pattern, DNA_string, d):
+    k = len(pattern)
+    return reduce(lambda x,y: x | y, [hamming_distance(pattern, DNA_string[i:i + k]) <= d for i in range(len(DNA_string) - k + 1)])
+
+
+def is_pattern_with_at_most_d__mistakes_in_DNA(pattern, DNA, d):
+    return reduce(lambda x,y: x & y, [is_pattern_with_at_most_d_mistakes_in_string(pattern, DNA[i], d) for i in range(len(DNA))])
+
+
+def mutations(pattern):
+    k = len(pattern)
+    return list({pattern[:i] + j + pattern[i+1:k] for i in range(0, k) for j in ["A", "C", "G", "T"]})
+
+
+def motif_enumeration(k, d, DNA):
+    patterns = []
+    for DNA_string in DNA:
+        for i in range(len(DNA[0]) - k + 1):
+            pattern_curr = DNA_string[i:i+k]
+            mutations_list = mutations(pattern_curr)
+            for mutation in mutations_list:
+                if is_pattern_with_at_most_d__mistakes_in_DNA(mutation, DNA, d):
+                    patterns.append(mutation)
+    return " ".join(list(set(patterns)))
+
+
 if __name__ == "__main__":
-    pass
+    k,d = tuple(map(int,input().split(" ")))
+    DNA = [input() for i in range(4)]
+    print(motif_enumeration(k, d, DNA))
